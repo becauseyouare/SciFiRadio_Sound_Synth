fe85d0baa8488633984713e426fa1fd4f2b6e997
becauseyouare <atomicwire@gmail.com>
2025-02-18 19:48:08 -0600

    more tweaking on sound and printout

--- build/sketch/SciFiRadio_Sound_Synth.ino.cpp
+++ build/sketch/SciFiRadio_Sound_Synth.ino.cpp
@@ -24,7 +24,7 @@ EventDelay PrintCadence;
 // int freqVal;
 const int carrFreq_M[] = {22,330,30,800,200,20,200,100,40,20,30,80,20,100,400,300,20,80,90,400,30,50};  // 22 to 880
 const int ldr1_M[] =     {70,300,100,50,400,100,80,700,30,60,10,80,200,300,100,40,10,60,90,200,70}; // 10 to 700
-const int modSp_M[] =    {1,50,9000,600,10,90,500,1000,200,500,800,30,90,100,200,700,30,80,100,300,10}; // float (1 to 10000)/1000
+const int modSp_M[] =    {1,5000,9000,600,100,90,500,10000,2000,500,800,30,90,100,200,7000,30,80,100,300,10}; // float (1 to 10000)/1000
 const int frq_M[] =      {10,04,8,10,06,04,01,01,06,06,07,01,03,02,01,02,10,02,07,05,8,9,10,10};  // 0 to 10
 const int knob2_M[] =    {0,01,03,02,07,06,9,03,04,10,00,03,02,02,01,02,06,07,8,03,00,00};  // 0 to 10
  
@@ -62,6 +62,7 @@ Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aCarrier(COS2048_DATA);
 Oscil<COS2048_NUM_CELLS, AUDIO_RATE> aModulator(COS2048_DATA);
 Oscil<COS2048_NUM_CELLS, CONTROL_RATE> kIntensityMod(COS2048_DATA);
 
+bool printMap = 0;
 int dialPosition = 0;
 int dialHist = 0;
 int mod_ratio = 5; // brightness (harmonics)
@@ -73,15 +74,15 @@ float smoothness = 0.95f;
 Smooth <long> aSmoothIntensity(smoothness);
 
 
-#line 75 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
+#line 76 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
 void setup();
-#line 86 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
+#line 88 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
 void updateControl();
-#line 179 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
+#line 197 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
 int updateAudio();
-#line 184 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
+#line 202 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
 void loop();
-#line 75 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
+#line 76 "C:\\Users\\atomi\\Dropbox\\MyProjects2022-2025\\SciFiRadio_Sound_Synth\\SciFiRadio_Sound_Synth.ino"
 void setup(){
   Serial.begin(115200); // set up the Serial output so we can look at the light level
   Serial.println("SciFiRadio_Sound_Synth.ino 2/15/2025 TomMcGuire");
@@ -89,6 +90,7 @@ void setup(){
   pinMode(2,INPUT_PULLUP);
   pinMode(3,INPUT_PULLUP);
   pinMode(4,INPUT_PULLUP);
+  if(!digitalRead(3)) printMap = 1;  // if pin D3 is grounded print the Synth profile map 0 to 1024
   PrintCadence.set(60); // set the print rate at 60ms
 }
 
@@ -116,7 +118,7 @@ void updateControl(){
   int x = (dialPosition/10) %50;
   int carrier_freq = map(x,0,50,carrFreq_M[a],carrFreq_M[a+1]);
   int LDR1_value =  map(x,0,50,ldr1_M[a],ldr1_M[a+1]);
-  float mod_speed =  map(x,0,50,modSp_M[a],modSp_M[a+1])/1000;
+  float mod_speed =  map(x,0,50,modSp_M[a],modSp_M[a+1])/1000.0;
   int FRQ =  map(x,0,50,frq_M[a],frq_M[a+1]);
   int knob2Val =  map(x,0,50,knob2_M[a],knob2_M[a+1]);
 
@@ -154,7 +156,23 @@ void updateControl(){
   if(PrintCadence.ready()){
     if(dialHist != (dialPosition/10)){
       dialHist = dialPosition/10;
-      Serial.println(dialPosition/10); //Send dial position to the SciFiRadio.py 
+      Serial.println(""); // prints a new line
+      Serial.print(dialPosition/10); //Send dial position to the SciFiRadio.py 
+    }
+    PrintCadence.start(); // reset the print rste timer
+    if(printMap){
+      if(dialPosition > 10200)printMap = 0;
+      Serial.print(","); // prints a tab
+      Serial.print(carrier_freq);
+      Serial.print(","); // prints a tab
+      Serial.print(LDR1_calibrated);
+      Serial.print(","); // prints a tab
+      Serial.print(mod_speed*10);
+      Serial.print(","); // prints a tab
+      Serial.print(FRQ*10);
+      Serial.print(","); // prints a tab
+      Serial.print(knob2Val*10);
+      dialPosition += 10;
     }
     PrintCadence.start(); // reset the print rste timer
   }
